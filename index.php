e79e33939e7f6e133a4c6229c07ad345867ca479
Marin <marinlarko@gmail.com>
2017-12-20 17:02:38 +0100

    zadaca

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<?php include_once "head.php";  ?>
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<?php include_once "zaglavlje.php"; ?>
+					<!-- Nav -->
+						<?php include_once "izbornik.php"; ?>
+
+			<!-- Banner -->
+				<?php include_once "banner.php"; ?>
+
+			<!-- Highlights -->
+				<?php include_once "tijelo.php"; ?>
+
+			<!-- Footer -->
+				<?php include_once "podnozje.php"; ?>
+
+		</div>
+
+		<!-- Scripts -->
+			<?php include_once "skripte.php"; ?>
+
+	</body>
+</html>
---
+++ zaglavlje.php
@@ -0,0 +1,5 @@
+<!-- Header -->
+				<div id="header">
+
+					<!-- Logo -->
+						<h1><a href="index.html" id="logo">Arcana <em>by HTML5 UP</em></a></h1>
